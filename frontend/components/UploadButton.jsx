3cae77796547fb10b90f735d9e139b8c32eba405
Aanchal Yadav <yadavaanchal2005@gmail.com>
2025-12-05 21:58:56 +0530

    Delete frontend/components directory

--- frontend/components/UploadButton.jsx
+++
@@ -1,8 +0,0 @@
-export default function UploadButton({onUpload}){
-  return (
-    <label className='cursor-pointer px-3 py-2 bg-white/10 rounded'>
-      Upload
-      <input type='file' className='hidden' onChange={e=>onUpload(e.target.files[0])}/>
-    </label>
-  )
-}
